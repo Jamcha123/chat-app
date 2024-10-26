fb7b156b22fd32c096150af11a0f8fbb332a1f09
james chambers <jamcha123@localhost.localdomain>
2024-10-26 11:04:34 +0200

    initalize the project

---
+++ index.js
@@ -0,0 +1,4 @@
+import {} from 'socket.io';
+import fs from 'fs';
+import {} from 'http';
+import express from 'express';
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./web1/index.html",
+    "./web2/index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
